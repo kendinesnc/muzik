ed87d2569e644115355e452863a0d47aca708738
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-05-26 18:02:55 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -8,13 +8,17 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""Hoşgeldin **Ben Netd Music 📀** Grubunuzda Şarkı Arayabilirim Stabil Çalışmam İçin Bana Herhangi Bir Yetki Verin 👁
-Kullanım Şekli : **/e < şarkı adı>** Örnek : /e suspus
+        f"""Hoşgeldin **Ben Netd Music 📀** Grubunuzda Şarkı Arayabilirim Stabil Çalışmam İçin Bana Herhangi Bir Yetki Verin 👁 
+ Kullanım Şekli : **/e < şarkı adı>** Örnek : /e suspus
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
+                    InlineKeyboardButton(
+                        "Sahibi 💬", url="https://t.me/Azerbesk"
+                    ),
+                    InlineKeyboardButton(
+                        "Kanal 📣", url="https://t.me/KaybedenlerOrkestrasi"
+                    )
                 ]
             ]
-        )
-    )
