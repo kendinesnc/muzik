8cee7379761202c59fc35a0651e595c75f391dba
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-05-26 23:56:14 +0300

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -8,24 +8,17 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""Ben **{bn}** !!
-Grubunuzun sesli sohbetinde müzik çalmanıza izin 😉
-Şu anda desteklediğim komutlar şunlardır:
-⚜️ /oynat - __Yanıtlanan ses dosyasını veya YouTube videosunu bağlantı üzerinden çalar.__
-⚜️ /durdur - __Sesli Sohbet Müziğini Duraklat.__
-⚜️ /devam - __Sesli Sohbet Müziğine Devam Et.__
-⚜️ /atla - __Sesli Sohbette Çalan Geçerli Müziği Atlar.__
-⚜️ /bitir - __Sırayı temizler ve Sesli Sohbet Müziği'ni sona erdirir.__
-⚜️ /bul - __Müziği bulup gruba gönderir. Örnek /bul tuğkan kusura bakma.__
+        f"""Ben **{bn}**
+Kullanım : /e < Şarkı Adı >
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Grup 💬", url="https://t.me/Saygisizlar"
+                        "Sahibi 💬", url="https://t.me/Azerbesk"
                     ),
                     InlineKeyboardButton(
-                        "Kanal 📣", url="https://t.me/SaygisizlarMusic"
+                        "Kanal 📣", url="https://t.me/KaybedenlerOrkestrasi"
                     )
                 ]
             ]
