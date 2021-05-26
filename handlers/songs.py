f1676849098e1ff019ff5b7c7ca29f53c0bbf0d6
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-05-27 01:32:17 +0300

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -55,13 +55,13 @@ async def a(client, message: Message):
         )
         print(str(e))
         return
-    await m.edit(f"🦸🏾 İndiriyorum... **Aranan :** {query}")
+    await m.edit(f"🦸🏾 İndiriyorum... **Aranan :**{query}")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'♥️ @DJnetdBot 🎶'
+        rep = f'♥️ @KaybedenlerOrkestrasi 🎶'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
